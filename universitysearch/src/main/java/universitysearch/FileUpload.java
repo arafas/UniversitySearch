348f19711e5ab6da5d991a83f0263efb3b7ee54c
JacobSteele <jacob.steele@mail.utoronto.ca>
2016-03-11 15:37:18 -0500

    Adding CMD feature for uploading files

--- universitysearch/src/main/java/universitysearch/FileUpload.java
+++ universitysearch/src/main/java/universitysearch/FileUpload.java
@@ -1,10 +1,12 @@
 package universitysearch;
 
+import com.sun.jersey.core.header.ContentDisposition;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.hibernate.SessionFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -19,9 +21,13 @@ public class FileUpload {
         File file2 = new File(uploads, contentDispositionHeader.getFileName());
         try {
             long fileSize = Files.copy(fileInputStream, file2.toPath());
-            String digestString = DigestUtils.md5Hex(file2.toString());
+            FileInputStream fileHash = new FileInputStream(file2);
+            String digestString = DigestUtils.md5Hex(fileHash);
             String filePathHash = DigestUtils.md5Hex(System.getenv("OPENSHIFT_DATA_DIR"));
-            String obfuscatedFilePath = filePathHash + "/" + file2.getName();
+            String filePath = file2.getAbsolutePath().
+                substring(0,file2.getAbsolutePath().lastIndexOf(File.separator));
+         // get the path of the file without the filename
+            String obfuscatedFilePath = filePathHash + filePath.replace(System.getenv("OPENSHIFT_DATA_DIR"), "").replace("\\", "/") + "/";
 
             FileManager fm = new FileManager();
             // Aquire DB connection
@@ -36,5 +42,45 @@ public class FileUpload {
             e.printStackTrace();
         }
     }
+    
+    public void saveFile(File fileInput, FileManager fm, String coursePath, int userID) {
+      // Method used for saving files from CMD line
+      
+      File uploads = new File(System.getenv("OPENSHIFT_DATA_DIR") + coursePath);
+      File file2 = new File(uploads, fileInput.getName());
+      FileInputStream fis = null;
+      try {
+          fis = new FileInputStream(fileInput);
+          boolean success = (new File(file2.getAbsolutePath().substring(0,file2.getAbsolutePath().lastIndexOf(File.separator)))).mkdirs();
+          System.out.println(success);
+          Files.copy(fis, file2.toPath());
+          long fileSize = fileInput.length();
+          String filePath = fileInput.getAbsolutePath().
+              substring(0,fileInput.getAbsolutePath().lastIndexOf(File.separator));
+          // get the path of the file without the filename
+          String fileName = fileInput.getName();
+          FileInputStream fileHash = new FileInputStream(fileInput);;
+          String fileCheck = DigestUtils.md5Hex(fileHash);
+          // get the md5 of the content in the file
+          String filePathHash = DigestUtils.md5Hex(System.getenv("OPENSHIFT_DATA_DIR"));
+
+          String obfuscatedFilePath = filePathHash  + coursePath;
+          // obscure the path to the data directory
+          
+          System.out.println(fileSize);
+          System.out.println(filePath.replace("\\", "/"));
+          System.out.println(fileName);
+          System.out.println(fileCheck);
+          System.out.println(obfuscatedFilePath);
+          
+          int res = fm.addFile(fileName, obfuscatedFilePath, "uploadedFile", fileCheck, fileSize, userID);
+          fis.close();
+          fileHash.close();
+          fis.close();
+
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
 }
 
