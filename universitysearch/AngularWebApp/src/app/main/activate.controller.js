f21746e111582884f2b85497f686c864502e60d0
pourcyrous <sampourcyrous1@hotmail.com>
2016-03-14 02:39:17 -0400

    fixed half the errors after merge conflicts

--- universitysearch/AngularWebApp/src/app/main/activate.controller.js
+++ universitysearch/AngularWebApp/src/app/main/activate.controller.js
@@ -1,25 +1,24 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('angularWebApp')
-    .controller('ActivateController', ActivateController);
+	angular.module('angularWebApp').controller('ActivateController',
+			ActivateController);
 
-  /** @ngInject */
-  
-  function ActivateController($scope, $http, $routeParams) {
-      var email = $routeParams.email;
-      var hash = $routeParams.hash;
+	/** @ngInject */
 
-      var url = 'rest/hello/activate/email/' + email + '/hash/' + hash;
+	function ActivateController($scope, $http, $routeParams) {
+		var vm = this;
 
-      $http.get(url)
-              .success(function(data, status, headers, config) {
-                $scope.activateResponse = data;
-                })
-                .error(function(data, status, headers, config) {
-                alert("AJAX failed to get data, status=" + status);
-              });
-    }
-    
+		var email = $routeParams.email;
+		var hash = $routeParams.hash;
+
+		var url = 'rest/hello/activate/email/' + email + '/hash/' + hash;
+
+		$http.get(url).success(function(data, status, headers, config) {
+			vm.activateResponse = data;
+		}).error(function(data, status, headers, config) {
+			alert("AJAX failed to get data, status=" + status);
+		});
+	}
+
 })();
