6c7b0c48ffe360f4b60bb546b1a3630ce47bc6a3
Zubair Baig <zubair.baig@mail.utoronto.ca>
2016-03-31 16:13:18 -0400

    pushing a bug in adding a course where it allows duplicates to be added to the local array

--- universitysearch/AngularWebApp/src/app/main/addDeleteCourse.controller.js
+++ universitysearch/AngularWebApp/src/app/main/addDeleteCourse.controller.js
@@ -8,30 +8,42 @@
         .module('angularWebApp')
         .controller('AddDeleteCourseController', AddDeleteCourseController);
     /** @ngInject */
-    function AddDeleteCourseController($http) {
+    function AddDeleteCourseController($http, $scope) {
         var vm = this;
         vm.courses = [];
         vm.selectedCourse ="";
+        vm.showAddCourseSuccess = false;
+        vm.showDeleteCourseSuccess = false;
 
         $http.get('/rest/API/coursesForProf')
             .then(function (resp) {
-                vm.courses = resp.data;
+                _.each(resp.data, function(course) {
+                    vm.courses.push(course);
+                });
             });
 
         vm.addCourse = function(course) {
             course.courseCode = course.courseCode.toUpperCase();
             return $http.post('rest/API/addCourse', course)
                 .then(function (resp) {
-                    vm.courses.push(resp.data);
+                    vm.showAddCourseSuccess = true;
+                    if (_.findWhere(vm.courses, {courseCode: resp.data.courseCode}) == null) {
+                        vm.courses.push(resp.data);
+                    }
+                    $scope.course.courseDesc = "";
+                    $scope.course.courseName = "";
+                    $scope.course.courseCode = "";
                 });
         };
 
         vm.deleteCourse = function() {
             return $http.post('rest/API/deleteCourse/' + vm.selectedCourse.id)
                 .then(function (resp) {
-                    vm.courses = _.reject (vm.courses, function (course) {
+                    vm.courses = _.reject(vm.courses, function (course) {
                         return (course.id == vm.selectedCourse.id);
-                    })
+                    });
+                    vm.showDeleteCourseSuccess = true;
+                    vm.selectedCourse = "";
                 });
         };
 
